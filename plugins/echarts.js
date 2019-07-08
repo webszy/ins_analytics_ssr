943a3f656af7e485a9f29145c3cfc55edf7ef83f
webszy <sunzhenyu@live.com>
2019-07-08 14:50:44 +0800

    新增wordCloud词云插件

--- plugins/echarts.js
+++ plugins/echarts.js
@@ -2,4 +2,10 @@
 import Vue from 'vue'
 // const echarts = require('echarts');
 import echarts from 'echarts'
-Vue.prototype.$echarts=echarts
+// import'echarts-wordcloud'
+if (process.client) {
+  window.echarts=echarts
+  require('echarts-wordcloud')
+  Vue.prototype.$echarts= window.echarts
+}
+
