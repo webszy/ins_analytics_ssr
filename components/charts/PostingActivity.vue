9b9205879a978f5d7150ec1c89b51b2acafd2351
webszy <sunzhenyu@live.com>
2019-07-11 19:33:38 +0800

    热力图调色

--- components/charts/PostingActivity.vue
+++ components/charts/PostingActivity.vue
@@ -26,11 +26,21 @@ return {
     title:{
       text:'Posting Activity',
       subtext:'Posting times on Profile',
-      left:'5%',  
+      left:'5%',
+      textStyle: {
+            color: "#050505",
+            fontSize: 24,
+            fontWeight:600,
+            fontFamily:'PingFangSC-Medium'
+        },
+        subtextStyle:{
+            color:'#7F7F7F',
+            fontSize: 16,
+        }  
     },
     grid: {
        left: '3%',
-          top: '10%',
+          top: '13%',
           right: '8%',
           bottom: '8%',
           containLabel: true
@@ -49,16 +59,37 @@ return {
             show: true
         }
      },
-    
+     dataZoom: [{
+        type: 'inside',
+        start: 0,
+        end: 10
+    }, {
+        start: 0,
+        end: 10,
+        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+        handleSize: '80%',
+        handleStyle: {
+            color: '#fff',
+            shadowBlur: 3,
+            shadowColor: 'rgba(0, 0, 0, 0.6)',
+            shadowOffsetX: 2,
+            shadowOffsetY: 2
+        },
+      
+    }],
     visualMap: [{
         show:true,
         min: 0,
         max: 10,
         range:[1,10],
         calculable: true,
+        // controller:true,
         orient: 'vertical',
         right: '2%',
-        bottom: '8%'
+        bottom: '8%',
+        inRange:{
+          color:['#F0F0F0','#3D4FD5','#26328F']
+        }
     }],
     series: [{
         name: 'Post Count',
