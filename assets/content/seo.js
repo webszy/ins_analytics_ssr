4c0810c0a38f91d80fa6377464d12cdebbf9fbee
webszy <sunzhenyu@live.com>
2019-07-16 14:57:55 +0800

    word change

--- assets/content/seo.js
+++ assets/content/seo.js
@@ -1,15 +1,15 @@
 module.exports= {
   name:'InsAnalysis',
-  subName:'INSTAGRAM AI ANALYTI',
+  subName:'INSTAGRAM AI Analytics',
   title:'Instagram Analytics For @Account & #Hashtag  | Free Tool | Get More Likes & Follower Easier',
   keywords:'influencer marketing,instagram analytics,instagram insights',
   description:'Search any @Account or #Hashtag to get instant Instagram analytics and insights. AI-powered metrics for Brand Marketers, Agencies, and Influencers',
   profilePage:{
-    title:'Instagram Analytics For @Account',
+    title:'Instagram Profile Analytics For @Account',
     keyWords:'',
   },
   hashtagPage:{
-    title:'',
+    title:'Instagram Hashtag Analytics For #Hashtag',
     keyWords:'',
   }
 }
