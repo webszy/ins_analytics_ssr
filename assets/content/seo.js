9c82bbb9f22671dea3c0031cea8f5380895ef54b
webszy <sunzhenyu@live.com>
2019-07-18 19:51:46 +0800

    新增关键字

--- assets/content/seo.js
+++ assets/content/seo.js
@@ -2,7 +2,7 @@ module.exports= {
   name:'InsAnalysis',
   subName:'INSTAGRAM AI Analytics',
   title:'Instagram Analytics For @Account & #Hashtag  | Free Tool | Get More Likes & Follower Easier',
-  keywords:'influencer marketing,instagram analytics,instagram insights',
+  keywords:'influencer marketing,instagram analytics,instagram insights,famedgram analytics',
   description:'Search any @Account or #Hashtag to get instant Instagram analytics and insights. AI-powered metrics for Brand Marketers, Agencies, and Influencers',
   profilePage:{
     title:'Instagram Profile Analytics For @Account',
